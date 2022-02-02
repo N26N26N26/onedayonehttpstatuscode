09b9153018ae4ff4e5f6543af8f9ee0dc1b2e273
N26N26N26 <jordan.verreaux@gmail.com>
2022-02-02 16:36:47 +0100

    deleted datetime

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(StatusRepository $statusRepository, Random $random): Response
     {
-        $today = new DateTime("now");
         $status = $statusRepository->findOneById($random->random());
 
         return $this->render('home/index.html.twig', [
