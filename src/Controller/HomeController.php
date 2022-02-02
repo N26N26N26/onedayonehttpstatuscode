87d585d9f769e3f2a5601269488a63eece581588
N26N26N26 <jordan.verreaux@gmail.com>
2022-02-02 16:13:47 +0100

    service created with a simple random between key

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\StatusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\Random;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(StatusRepository $statusRepository, Random $random): Response
     {
-        return $this->render('home/index.html.twig');
+        $status = $statusRepository->findOneById($random->random());
+
+        return $this->render('home/index.html.twig', [
+        'status' => $status]);
     }
 }
---
+++ src/Service/Random.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+class Random
+{
+    public function random()
+    {
+        return rand(84, 166);
+    }
+}
