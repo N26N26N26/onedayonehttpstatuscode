f08bdd44cab5dd658072b8187d786a8f4d304939
N26N26N26 <jordan.verreaux@gmail.com>
2022-02-02 12:28:59 +0100

    added Entity Status

---
+++ src/Entity/Status.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StatusRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StatusRepository::class)]
+class Status
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $code;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $message;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $signification;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getSignification(): ?string
+    {
+        return $this->signification;
+    }
+
+    public function setSignification(string $signification): self
+    {
+        $this->signification = $signification;
+
+        return $this;
+    }
+}
